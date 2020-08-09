55f8ab3543147268af4f54ad057e8d95dea608c7
MBadman <69422097+MBadman@users.noreply.github.com>
2020-08-09 15:21:34 +0300

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,1 +1,14 @@
-
+class CfgPatches
+{
+	class rb_rd54_radio_fix
+    	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[]={"40th_backpack"};
+		version = "1";
+		projectName = "rd54fix";
+		author = "bolov1999@mail.ru";
+	};
+  class CfgVehicles{
+          class
