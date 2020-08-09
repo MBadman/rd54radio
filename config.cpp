e6e21742540b2a8981b354b9cd6bd1b7b1d9afe5
MBadman <69422097+MBadman@users.noreply.github.com>
2020-08-09 15:55:24 +0300

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -12,9 +12,9 @@ class CfgPatches
 	};
 };
   class CfgVehicles{
-          class {Bag_Base};
+          class Bag_Base{};
 	  
-          class RD54_6B3: {Bag_Base};
+          class RD54_6B3: Bag_Base{};
 		  
           class RD54_6B3_R148: RD54_6B3{
 		  tf_dialog = "mr3000_radio_dialog";
